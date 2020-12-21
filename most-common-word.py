672f08f1b24fef695cf6e8e7597dcf1da9760548
박도희 <dohee.pa@gmail.com>
2020-12-21 14:29:15 +0900

    CREATE | 문자열 조작

---
+++ most-common-word.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        banned = set(banned)
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
+        counts = defaultdict(int)
+        for word in words:
+            counts[word] += 1
+        return max(counts, key=counts.get)
