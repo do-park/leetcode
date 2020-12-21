cb50d1b009422dd600644f10b342837c79f72fbb
박도희 <dohee.pa@gmail.com>
2020-12-21 14:39:26 +0900

    UPDATE | defaultdict to Counter in collections

--- most-common-word.py
+++ most-common-word.py
@@ -1,10 +1,7 @@
-from collections import defaultdict
+from collections import Counter
 
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         banned = set(banned)
         words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
-        counts = defaultdict(int)
-        for word in words:
-            counts[word] += 1
-        return max(counts, key=counts.get)
+        return Counter(words).most_common()[0][0]
