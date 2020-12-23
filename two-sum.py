5d9259de2a5fca56f7ff9d561236621433f3a392
박도희 <dohee.pa@gmail.com>
2020-12-23 22:16:19 +0900

    UPDATE | 딕셔너리, 선형자료구조

--- two-sum.py
+++ two-sum.py
@@ -1,13 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums = sorted([(num, i) for i, num in enumerate(nums)])
-        left, right = 0, len(nums) - 1
-        num, idx = 0, 1
-        while not left == right:
-            temp = nums[left][num] + nums[right][num]
-            if temp > target:
-                right -= 1
-            elif temp < target:
-                left += 1
-            else:
-                return [nums[left][idx], nums[right][idx]]
+        nums_map = {}
+        for i, num in enumerate(nums):
+            if target - num in nums_map:
+                return [nums_map[target - num], i]
+            nums_map[num] = i
