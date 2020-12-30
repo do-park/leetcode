82a3bdb3fdfe31e7aba5cebe1438cd79016a2f53
박도희 <dohee.pa@gmail.com>
2020-12-30 11:50:42 +0900

    CREATE | 배열

---
+++ product-of-array-except-self.py
@@ -0,0 +1,12 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = []
+        p = 1
+        for i in range(len(nums)):
+            result.append(p)
+            p *= nums[i]
+        p = 1
+        for i in range(len(nums) - 1, -1, -1):
+            result[i] *= p
+            p *= nums[i]
+        return result
