5cebc09d6c8cf141df6704df853313072654a6dd
박도희 <dohee.pa@gmail.com>
2021-01-03 00:46:02 +0900

    UPDATE | linked list, repeating

--- reverse-linked-list.py
+++ reverse-linked-list.py
@@ -5,10 +5,8 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        def reverse(node: ListNode, prev: ListNode = None):
-            if not node:
-                return prev
+        node, prev = head, None
+        while node:
             next, node.next = node.next, prev
-            return reverse(next, node)
-
-        return reverse(head)
+            prev, node = node, next
+        return prev
